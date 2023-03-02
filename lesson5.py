913c8e331294c3ab1a1bb9bb228f51e47e42c98b
RomanKuzhel <royal.manroman@gmail.com>
2023-03-02 22:27:33 +0000

    123

---
+++ lesson5.py
@@ -0,0 +1,46 @@
+# string="qwertyui"
+# print(string[3])
+
+# string="qwertyui"
+# print(string[-2])
+
+# string="qwertyui"
+# print(string[:5:])
+
+# string = "qwertyui"
+# print(string[:6])
+
+# string = "qwertyui"
+# print(string[::2])
+
+# string = "qwertyui"
+# print(string[1::2])
+
+# string = "qwertyui"
+# print(string[::-1])
+
+# string = "qwertyui"
+# print(string[::-2])
+
+# string = "qwertyui"
+# print(len(string))
+
+# string = 'Это пример строки '
+# words = string.count(' ')
+# print(words)
+
+# s = 'qwertychuchqwerchtycht'
+# word = -1
+# count = 0
+# while True:
+# word = s.find("ch", word + 1)
+# if word == -1:
+#     break
+# count += 1
+# print(count)
+
+#string = "hhhhhhhhhh"
+#first_l = string.find('h')
+#last = string.rfind('h')
+#my_str = string[0:first_l + 1] + string[first_l + 1:last].replace('h', 'H') + string[last:]
+#print(my_str)
